43fff0ebbd5725955871e1b86c8708d606b79dc4
nmacawile <nmacawile@gmail.com>
2025-12-17 15:52:11 +0800

    Expose firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBi3NC8cgHeEiEh9CMx25BNtEQSVlZuV6Q",
+  authDomain: "merry-christmas-66ae0.firebaseapp.com",
+  projectId: "merry-christmas-66ae0",
+  storageBucket: "merry-christmas-66ae0.firebasestorage.app",
+  messagingSenderId: "12886564036",
+  appId: "1:12886564036:web:edefd87ac97ca58f43fd91"
+};
+export default firebaseConfig;
